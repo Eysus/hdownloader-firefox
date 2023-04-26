87cfdc5387bb3d4588a04d85b4a249f195e6b099
Eysus <eysus@protonmail.com>
2023-04-26 23:18:36 +0200

    feat: add bulk download for nhentai galleries

--- js/javguru.js
+++ js/javguru.js
@@ -1,27 +1,29 @@
-// Check if it's the jav page or not
-if (window.location.pathname.match(/^\/\d+/)) {
-    const title = document.getElementsByTagName("h1")[0];
-    const code = title.textContent.match(/\[(.*?)\]/)[1];
-    const url = `https://sukebei.nyaa.si/?f=0&c=0_0&q=${code}&s=seeders&o=desc`;
+if (window.location.hostname == 'jav.guru') {
+  if (window.location.pathname.match(/^\/\d+/)) {
+    const title = document.getElementsByTagName('h1')[0]
+    const code = title.textContent.match(/\[(.*?)\]/)[1]
+    const url = `https://sukebei.nyaa.si/?f=0&c=0_0&q=${code}&s=seeders&o=desc`
 
-    const container = document.getElementsByClassName("large-screenshot")[0];
-    container.style = "position: relative;";
-    let a = document.createElement('a');
-    a.title = "Download gallery";
-    a.href = url;
-    a.className = "hbtn hbtn-link";
-    container.appendChild(a);
+    const container = document.getElementsByClassName('large-screenshot')[0]
+    container.style = 'position: relative;'
+    let a = document.createElement('a')
+    a.title = 'Download gallery'
+    a.href = url
+    a.className = 'hbtn hbtn-link'
+    container.appendChild(a)
+  } else {
+    for (let element of document.getElementsByClassName('row')) {
+      const code = element
+        .getElementsByTagName('h2')[0]
+        .textContent.match(/\[(.*?)\]/)[1]
+      const url = `https://sukebei.nyaa.si/?f=0&c=0_0&q=${code}&s=seeders&o=desc`
 
-} else {
-    for (let element of document.getElementsByClassName("row")) {
-        const code = element.getElementsByTagName("h2")[0].textContent.match(/\[(.*?)\]/)[1];
-        const url = `https://sukebei.nyaa.si/?f=0&c=0_0&q=${code}&s=seeders&o=desc`;
+      let a = document.createElement('a')
+      a.title = 'Download gallery'
+      a.href = url
+      a.className = 'hbtn hbtn-link'
 
-        let a = document.createElement('a');
-        a.title = "Download gallery";
-        a.href = url;
-        a.className = "hbtn hbtn-link";
-
-        element.appendChild(a);
+      element.appendChild(a)
     }
+  }
 }
--- js/nhentai.js
+++ js/nhentai.js
@@ -1,14 +1,47 @@
-for (let element of document.getElementsByClassName("cover")) {
-    const url = element["href"] + "download";
-    let a = document.createElement('a');
-    a.title = "Download gallery";
-    a.href = url;
-    a.style = "position: absolute";
-    a.className = "btn btn-primary";
+if (window.location.hostname == 'nhentai.net') {
+  const urls = []
 
-    let i = document.createElement('i');
-    i.className = "fa fa-download";
-    a.appendChild(i);
+  for (let element of document.getElementsByClassName('cover')) {
+    const url = element['href'] + 'download'
+    if (!element.parentElement.classList.contains('blacklisted')) {
+      urls.push(url)
+    }
+    element.appendChild(createButton(url, 'Download gallery'))
+  }
 
-    element.appendChild(a);
+  document
+    .getElementById('content')
+    .prepend(createButton('', 'Bulk', 'Bulk', true))
+
+  function createButton(url, title, text = '', bulk = false) {
+    let linkElement = document.createElement('a')
+    linkElement.title = title
+    linkElement.style = 'position: absolute'
+    linkElement.className = 'btn btn-primary'
+
+    if (bulk) {
+      linkElement.onclick = function () {
+        bulkDownload()
+      }
+    } else {
+      linkElement.href = url
+    }
+
+    let icon = document.createElement('i')
+    icon.className = 'fa fa-download'
+    linkElement.appendChild(icon)
+    if (text != '') {
+      linkElement.append(' ' + text)
+    }
+
+    return linkElement
+  }
+
+  async function bulkDownload() {
+    for (let url of urls) {
+      console.log('Downloading ' + url + '...')
+      window.open(url)
+      await new Promise((r) => setTimeout(r, 1000))
+    }
+  }
 }
