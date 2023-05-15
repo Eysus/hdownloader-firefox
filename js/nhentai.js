909f7f5bfc8570698c1808425e3bc66554232101
Eysus <75269165+Eysus@users.noreply.github.com>
2023-05-15 20:20:18 +0200

    feat: bulk auto (#8)

--- js/nhentai.js
+++ js/nhentai.js
@@ -1,47 +1,105 @@
 if (window.location.hostname == 'nhentai.net') {
-  const urls = []
+  const buttons = [];
 
   for (let element of document.getElementsByClassName('cover')) {
     const url = element['href'] + 'download'
+    let newButton = createButton(url, 'Download gallery');
+    element.appendChild(newButton)
     if (!element.parentElement.classList.contains('blacklisted')) {
-      urls.push(url)
+      buttons.push(newButton);
     }
-    element.appendChild(createButton(url, 'Download gallery'))
   }
 
-  document
-    .getElementById('content')
-    .prepend(createButton('', 'Bulk', 'Bulk', true))
+  let icon = document.createElement('i')
+  icon.className = 'fa fa-download'
 
-  function createButton(url, title, text = '', bulk = false) {
+  let bulkBox = document.createElement('div')
+  bulkBox.className = 'bulk-box'
+
+  let bulkButton = document.createElement('a')
+  bulkButton.title = 'Bulk'
+  bulkButton.className = 'btn btn-primary'
+  bulkButton.id = 'bulk-button'
+  bulkButton.appendChild(addIcon())
+  bulkButton.onclick = () => bulkDownload()
+  bulkButton.append(' Bulk')
+  bulkBox.appendChild(bulkButton)
+
+  let bulkAutoButton = document.createElement('a')
+  bulkAutoButton.title = 'Bulk Auto'
+  bulkAutoButton.className = 'btn btn-primary'
+  bulkAutoButton.id = 'bulk-auto-button'
+  bulkAutoButton.appendChild(addIcon())
+  bulkAutoButton.onclick = () => bulkDownloadAutoClick()
+  bulkAutoButton.append(' Bulk Auto')
+  bulkBox.appendChild(bulkAutoButton)
+
+  document.body.appendChild(bulkBox)
+
+  if (localStorage.getItem('auto') == 'true') {
+    bulkDownloadAuto()
+  }
+
+  // FUNCTIONS
+
+  function createButton(url, title) {
     let linkElement = document.createElement('a')
     linkElement.title = title
-    linkElement.style = 'position: absolute'
     linkElement.className = 'btn btn-primary'
-
-    if (bulk) {
-      linkElement.onclick = function () {
-        bulkDownload()
-      }
-    } else {
-      linkElement.href = url
-    }
-
-    let icon = document.createElement('i')
-    icon.className = 'fa fa-download'
-    linkElement.appendChild(icon)
-    if (text != '') {
-      linkElement.append(' ' + text)
-    }
+    linkElement.style = 'position: absolute'
+    linkElement.href = url
+    linkElement.appendChild(addIcon())
 
     return linkElement
   }
 
   async function bulkDownload() {
-    for (let url of urls) {
-      console.log('Downloading ' + url + '...')
-      window.open(url)
-      await new Promise((r) => setTimeout(r, 1000))
+    for (let btn of buttons) {
+      //btn.click();
+      window.open(btn.href);
+      window.focus();
+      await new Promise((r) => setTimeout(r, 2000))
     }
+  }
+
+  async function bulkDownloadAutoClick() {
+    if (localStorage.getItem('auto') == 'true') {
+      localStorage.setItem('auto', false)
+      document.getElementById('bulk-auto-button').textContent = ' Bulk auto'
+      bulkAutoButton.prepend(addIcon())
+    } else {
+      localStorage.setItem('auto', true)
+      bulkDownloadAuto()
+    }
+  }
+
+  async function bulkDownloadAuto() {
+    let sec = Math.floor(Math.random() * 60 + 60)
+    await new Promise((resolve) => {
+      let countdown = setInterval(() => {
+        if (localStorage.getItem('auto') == 'true') {
+          document.getElementById('bulk-auto-button').textContent =
+            ' Bulk auto (' + sec-- + ')'
+          if (sec < 0) {
+            resolve()
+            clearInterval(countdown)
+          }
+        } else {
+          resolve()
+          clearInterval(countdown)
+        }
+      }, 1000)
+    })
+
+    if (localStorage.getItem('auto') == 'true') {
+      await bulkDownload()
+      document.getElementsByClassName('next')[0].click()
+    }
+  }
+
+  function addIcon() {
+    let icon = document.createElement('i')
+    icon.className = 'fa fa-download'
+    return icon
   }
 }
