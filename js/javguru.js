72fc4c32de3648db5d1634b4980c77325fd6eb31
Eysus <eysus@protonmail.com>
2023-08-26 23:59:34 +0200

    fix(javguru): better manage link between guru and library for fc2

--- js/javguru.js
+++ js/javguru.js
@@ -1,29 +1,29 @@
-if (window.location.hostname == 'jav.guru') {
+if (window.location.hostname == "jav.guru") {
   if (window.location.pathname.match(/^\/\d+/)) {
-    const title = document.getElementsByTagName('h1')[0]
-    const code = title.textContent.match(/\[(.*?)\]/)[1]
-    const url = `https://sukebei.nyaa.si/?f=0&c=0_0&q=${code}&s=seeders&o=desc`
+    const title = document.getElementsByTagName("h1")[0];
+    let code = title.textContent.match(/\[(.*?)\]/);
+    code = code == null ? title.textContent : code[1];
 
-    const container = document.getElementsByClassName('large-screenshot')[0]
-    container.style = 'position: relative;'
-    let a = document.createElement('a')
-    a.title = 'Download gallery'
-    a.href = url
-    a.className = 'hbtn hbtn-link'
-    container.appendChild(a)
+    const div = document.createElement("div");
+    let a = getNyaaLink(code, "Nyaa");
+    a.className = "hbtn hbtn-link";
+    div.append(a);
+
+    const infoRight = document.getElementsByClassName("inforight")[0];
+    infoRight.appendChild(div);
   } else {
-    for (let element of document.getElementsByClassName('row')) {
+    for (let element of document.getElementsByClassName("row")) {
       const code = element
-        .getElementsByTagName('h2')[0]
-        .textContent.match(/\[(.*?)\]/)[1]
-      const url = `https://sukebei.nyaa.si/?f=0&c=0_0&q=${code}&s=seeders&o=desc`
+        .getElementsByTagName("h2")[0]
+        .textContent.match(/\[(.*?)\]/)[1];
+      const url = `https://sukebei.nyaa.si/?f=0&c=0_0&q=${code}&s=seeders&o=desc`;
 
-      let a = document.createElement('a')
-      a.title = 'Download gallery'
-      a.href = url
-      a.className = 'hbtn hbtn-link'
+      let a = document.createElement("a");
+      a.title = "Download gallery";
+      a.href = url;
+      a.className = "hbtn hbtn-corner hbtn-link";
 
-      element.appendChild(a)
+      element.appendChild(a);
     }
   }
 }
--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,5 @@
 sanitize = (str) => {
-    return str
+  return str
     .replace(/(((\(([^\)]+)\))|\s|(\[([^\]]+)\])|(\{([^\}]+)\}))*)$/, "")
     .replace(/^(((\(([^\)]+)\))|\s|(\[([^\]]+)\])|(\{([^\}]+)\}))*)/, "")
     .trim();
@@ -8,18 +8,37 @@ sanitize = (str) => {
 hostname = () => window.location.hostname;
 path = () => window.location.pathname;
 
- getNyaaSeedValue = (url) => {
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(data, "text/html");
-            const table = doc.getElementsByClassName("table")[0];
-            const firstLine = table.getElementsByTagName("tbody")[0].getElementsByTagName("tr")[0];
-            const seed = firstLine.getElementsByTagName("td")[5].innerText;
-            document.getElementById("hdown-seed").innerText = " (" + seed + ")";
-        })
-        .catch(error => {
-            console.log(error);
-        });    
-}
+getNyaaSeedValue = (url) => {
+  fetch(url)
+    .then((response) => response.text())
+    .then((data) => {
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(data, "text/html");
+      const table = doc.getElementsByClassName("table")[0];
+      const firstLine = table
+        .getElementsByTagName("tbody")[0]
+        .getElementsByTagName("tr")[0];
+      const seed = firstLine.getElementsByTagName("td")[5].innerText;
+      document.getElementById("hdown-seed").innerText = " (" + seed + ")";
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+getNyaaLink = (code, title = "") => {
+  const url = `https://sukebei.nyaa.si/?f=0&c=0_0&q=${code}&s=seeders&o=desc`
+
+  const seedValue = document.createElement("span");
+  seedValue.id = "hdown-seed";
+
+  let dlLink = document.createElement("a");
+  dlLink.textContent = title;
+  dlLink.href = url;
+
+  dlLink.appendChild(seedValue);
+
+  getNyaaSeedValue(url)
+  
+  return dlLink
+};
